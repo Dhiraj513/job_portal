b345fbcfd660749ab4a45ee4a93e1785e25455b0
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-05-05 19:54:52 +0545

    1.2

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AccountController extends Controller
+{
+    //this method will show user registration page
+    public function registration(){
+return view('front.account.registration');
+    }
+    //this will save a user
+public function processRegistration(Request $request) {
+
+    $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|min:5|confirmed',
+    ]);
+
+    if ($validator->passes()) {
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        session()->flash('success','you have registered successfully.');
+
+        return response()->json([
+            'status' => true,
+            'errors' => []
+        ]);
+    } else {
+        return response()->json([
+            'status' => false,
+            'errors' => $validator->errors()
+        ]);
+    }
+}
+
+    //this will show user login page
+public function login() {
+return view('front.account.login');
+}
+
+//auth
+public function authenticate(Request $request) {
+    $validator = Validator::make($request->all(),[
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+    if ($validator->passes()) {
+        if (Auth::attempt([
+            'email'=>$request->email,
+            'password' => $request->password
+             ])) {
+        return redirect()->route('account.profile');
+        } else{
+            return redirect()->route('account.login')->with('error','Either Email/Passsword is incorrect');
+
+        }
+
+    }
+    else{
+       return redirect()->route('account.login')
+       ->withErrors($validator)
+       ->withInput($request->only('email'));
+    }
+}
+public function profile() {
+    return view('front.account.profile');
+}
+public function logout() {
+    Auth::logout();
+    return redirect()->route('account.login');
+}
+
+};
