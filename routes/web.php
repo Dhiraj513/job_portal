1b474224f5e6ef2ddc83fbc6e91a325e43e2c91d
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-05-06 16:22:43 +0545

    1.4

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,33 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
 
-//Route::get('/', function () {
-  //  return view('welcome');
-//});
-Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
-Route::post('/account/process-register',[AccountController::class,'processRegistration'])->name('account.processRegistration');
-Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
-Route::post('/account/authenticate',[AccountController::class,'authenticate'])->name('account.authenticate');
-Route::get('/account/profile',[AccountController::class,'profile'])->name('account.profile');
-Route::get('/account.logout',[AccountController::class,'logout'])->name('account.logout');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-
+// Guest routes (only for NOT logged-in users)
 Route::middleware('guest')->group(function () {
-    Route::get('/account/login',[AccountController::class,'login'])->name('account.login');
-    Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
+
+    Route::get('/account/login', [AccountController::class, 'login'])
+        ->name('account.login');
+
+    Route::get('/account/register', [AccountController::class, 'registration'])
+        ->name('account.registration');
+
+    Route::post('/account/process-register', [AccountController::class, 'processRegistration'])
+        ->name('account.processRegistration');
+
+    Route::post('/account/authenticate', [AccountController::class, 'authenticate'])
+        ->name('account.authenticate');
 });
 
-Route::get('/account/profile',[AccountController::class,'profile'])
-    ->name('account.profile')
-    ->middleware('auth');
+// Auth routes (only for logged-in users)
+Route::middleware('auth')->group(function () {
 
+    Route::get('/account/profile', [AccountController::class, 'profile'])
+        ->name('account.profile');
 
+    Route::get('/account/logout', [AccountController::class, 'logout'])
+        ->name('account.logout');
 
-
-
+    Route::post('/account/update-profile', [AccountController::class, 'updateProfile'])
+        ->name('account.updateProfile');
+});
