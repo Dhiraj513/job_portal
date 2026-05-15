3426287175a78742075cc1bc2748ed3219a184e4
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-05-15 11:09:11 +0545

    2.1

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -5,19 +5,59 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\JobType;
 use App\Models\Category;
+use App\Models\Job;
 
 class JobsController extends Controller
 {
     //this method will show jobs page
-    public function index(){
+    public function index(Request $request){
 
     $categories = Category::where('status',1)->get();
-    $jobTypes = JobType::where('status',1)->get();
+    $jobTypes = jobType::where('status',1)->get();
 
+   $jobs= Job::where('status',1);
+
+   //search using keywords
+   if (!empty($request->keyword)) {
+    $jobs =$jobs->where(function($query)use ($request) {
+    $query->orWhere('title','like','%'.$request->keyword.'%');
+    $query->orWhere('keywords','like','%'.$request->keyword.'%');
+   });
+   }
+   //search using location
+   if(!empty($request->location)) {
+    $jobs = $jobs->where('location',$request->location);
+   }
+   //search using category
+   if(!empty($request->category)) {
+    $jobs = $jobs->where('category_id',$request->category);
+   }
+   //search using Job Type
+   $jobTypeArray = [];
+   if(!empty($request->jobType)) {
+    $jobTypeArray = explode(',', $request->jobType);
+    $jobs = $jobs->whereIn('job_type_id',$jobTypeArray);
+   }
+   //search using experience
+   if(!empty($request->experience)) {
+    $jobs = $jobs->where('experience',$request->experience);
+   }
+
+   $jobs= $jobs->with('jobType');
+
+   if(!empty($request->sort) && $request->sort == 0) {
+   $jobs= $jobs->orderBy('created_at','ASC');
+   }else {
+    $jobs= $jobs->orderBy('created_at','DESC');
+   }
+  $jobs= $jobs ->paginate(9);
+    
 
         return view('front.jobs',[
         'categories' => $categories,
         'jobTypes' => $jobTypes,
+        'jobs' => $jobs,
+        'jobTypeArray' => $jobTypeArray,
         ]);
     }
 }
--- resources/views/front/jobs.blade.php
+++ resources/views/front/jobs.blade.php
@@ -9,24 +9,24 @@
             <div class="col-6 col-md-2">
                 <div class="align-end">
                     <select name="sort" id="sort" class="form-control">
-                        <option value="">Latest</option>
-                        <option value="">Oldest</option>
+                        <option value="1">Latest</option>
+                        <option value="0">Oldest</option>
                     </select>
                 </div>
             </div>
         </div>
-
         <div class="row pt-5">
             <div class="col-md-4 col-lg-3 sidebar mb-4">
+                <form action="" name="searchform" id="searchForm">
                 <div class="card border-0 shadow p-4">
                     <div class="mb-4">
                         <h2>Keywords</h2>
-                        <input type="text" placeholder="Keywords" class="form-control">
+                        <input value="{{ Request::get('keyword') }}"  type="text" name="keyword" id="keyword" placeholder="Keywords" class="form-control">
                     </div>
 
                     <div class="mb-4">
                         <h2>Location</h2>
-                        <input type="text" placeholder="Location" class="form-control">
+                        <input value="{{ Request::get('location') }}"  type="text" name="location" id="location"  placeholder="Location" class="form-control">
                     </div>
 
                     <div class="mb-4">
@@ -35,7 +35,7 @@
                             <option value="">Select a Category</option>
                            @if($categories)
                            @foreach($categories as $category )
-                           <option value="{{ $category->id }}">{{ $category->name }}</option>
+                           <option {{ Request::get('category')==$category->id ?'selected':'' }} value="{{ $category->id }}">{{ $category->name }}</option>
                            @endforeach
                            @endif
                         </select>
@@ -45,60 +45,63 @@
                         <h2>Job Type</h2>
             @if($jobTypes->isNotEmpty())
             @foreach($jobTypes as $jobType)
-    
-            @endforeach
-     <div class="form-check mb-2"> 
-                            <input class="form-check-input " name="job_type" type="checkbox" value="{{ $jobType->id}}" id="job-type-{{ $jobType->id}}">    
-                            <label class="form-check-label " for="job-type-{{ $jobType->id}}">{{ $jobType->name }}</label>
-                        </div>
-            @endif
-
-                       
-                    </div>
-
-                        
-
-                    <div class="mb-4">
+    <div class="form-check mb-2"> 
+      <input {{ (in_array($jobType->id,$jobTypeArray)) ? 'checked' : ''}} class="form-check-input " name="job_type" type="checkbox" value="{{ $jobType->id}}" id="job-type-{{ $jobType->id}}">    
+     <label class="form-check-label " for="job-type-{{ $jobType->id}}">{{ $jobType->name }}</label>
+     </div>
+     @endforeach
+     @endif
+ </div>
+                <div class="mb-4">
                         <h2>Experience</h2>
-                        <select name="category" id="category" class="form-control">
+                        <select name="experience" id="experience" class="form-control">
                             <option value="">Select Experience</option>
-                            <option value="">1 Year</option>
-                            <option value="">2 Years</option>
-                            <option value="">3 Years</option>
-                            <option value="">4 Years</option>
-                            <option value="">5 Years</option>
-                            <option value="">6 Years</option>
-                            <option value="">7 Years</option>
-                            <option value="">8 Years</option>
-                            <option value="">9 Years</option>
-                            <option value="">10 Years</option>
-                            <option value="">10+ Years</option>
+                            <option value="1"{{ (Request::get('experience')==1) ? 'selected' :' ' }}>1 Year</option>
+                            <option value="2"{{ (Request::get('experience')==2) ? 'selected' :' ' }}>2 Years</option>
+                            <option value="3"{{ (Request::get('experience')==3) ? 'selected' :' ' }}>3 Years</option>
+                            <option value="4"{{ (Request::get('experience')==4) ? 'selected' :' ' }}>4 Years</option>
+                            <option value="5"{{ (Request::get('experience')==5) ? 'selected' :' ' }}>5 Years</option>
+                            <option value="6"{{ (Request::get('experience')==6) ? 'selected' :' ' }}>6 Years</option>
+                            <option value="7"{{ (Request::get('experience')==7) ? 'selected' :' ' }}>7 Years</option>
+                            <option value="8"{{ (Request::get('experience')==8) ? 'selected' :' ' }}>8 Years</option>
+                            <option value="9"{{ (Request::get('experience')==9) ? 'selected' :' ' }}>9 Years</option>
+                            <option value="10"{{ (Request::get('experience')==10)? 'selected' :' ' }}>10 Years</option>
+                            <option value="10_plus"{{(Request::get('experience')=='10_plus') ? 'selected' :' ' }} >10+ Years</option>
                         </select>
-                    </div>                    
+                    </div> 
+                    <button type="submit" class="btn btn-primary">Search</button>                   
                 </div>
+            </form>
             </div>
             <div class="col-md-8 col-lg-9 ">
                 <div class="job_listing_area">                    
                     <div class="job_lists">
                     <div class="row">
-                        <div class="col-md-4">
+                        @if($jobs->isNotEmpty())
+                        @foreach($jobs as $job)
+                             <div class="col-md-4">
                             <div class="card border-0 p-3 shadow mb-4">
                                 <div class="card-body">
-                                    <h3 class="border-0 fs-5 pb-2 mb-0">Web Developer</h3>
-                                    <p>We are in need of a Web Developer for our company.</p>
+                                    <h3 class="border-0 fs-5 pb-2 mb-0">{{ $job->title }}</h3>
+                                    <p>{{ Str::words ($job->description, $words=10, '...') }}</p>
                                     <div class="bg-light p-3 border">
                                         <p class="mb-0">
                                             <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                            <span class="ps-1">Noida</span>
+                                            <span class="ps-1">{{ $job->location }}</span>
                                         </p>
                                         <p class="mb-0">
                                             <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                            <span class="ps-1">Remote</span>
+                                            <span class="ps-1">{{ $job->jobType->name }}</span>
                                         </p>
+                                        <p>Keywords: {{ $job->keywords }} </p>
+                                        <p>Category: {{ $job->category->name }} </p>
+                                        <p>Experience: {{ $job->experience }} </p>
+                                        @if (!is_null($job->salary))
                                         <p class="mb-0">
                                             <span class="fw-bolder"><i class="fa fa-usd"></i></span>
-                                            <span class="ps-1">2-3 Lacs PA</span>
+                                            <span class="ps-1">{{ $job->salary }}</span>
                                         </p>
+                                        @endif
                                     </div>
 
                                     <div class="d-grid mt-3">
@@ -106,139 +109,11 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        
-                        <div class="col-md-4">
-                            <div class="card border-0 p-3 shadow mb-4">
-                                <div class="card-body">
-                                    <h3 class="border-0 fs-5 pb-2 mb-0">Web Developer</h3>
-                                    <p>We are in need of a Web Developer for our company.</p>
-                                    <div class="bg-light p-3 border">
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                            <span class="ps-1">Noida</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                            <span class="ps-1">Remote</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-usd"></i></span>
-                                            <span class="ps-1">2-3 Lacs PA</span>
-                                        </p>
-                                    </div>
-
-                                    <div class="d-grid mt-3">
-                                        <a href="job-detail.html" class="btn btn-primary btn-lg">Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card border-0 p-3 shadow mb-4">
-                                <div class="card-body">
-                                    <h3 class="border-0 fs-5 pb-2 mb-0">Web Developer</h3>
-                                    <p>We are in need of a Web Developer for our company.</p>
-                                    <div class="bg-light p-3 border">
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                            <span class="ps-1">Noida</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                            <span class="ps-1">Remote</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-usd"></i></span>
-                                            <span class="ps-1">2-3 Lacs PA</span>
-                                        </p>
-                                    </div>
-
-                                    <div class="d-grid mt-3">
-                                        <a href="job-detail.html" class="btn btn-primary btn-lg">Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card border-0 p-3 shadow mb-4">
-                                <div class="card-body">
-                                    <h3 class="border-0 fs-5 pb-2 mb-0">Web Developer</h3>
-                                    <p>We are in need of a Web Developer for our company.</p>
-                                    <div class="bg-light p-3 border">
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                            <span class="ps-1">Noida</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                            <span class="ps-1">Remote</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-usd"></i></span>
-                                            <span class="ps-1">2-3 Lacs PA</span>
-                                        </p>
-                                    </div>
-
-                                    <div class="d-grid mt-3">
-                                        <a href="job-detail.html" class="btn btn-primary btn-lg">Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card border-0 p-3 shadow mb-4">
-                                <div class="card-body">
-                                    <h3 class="border-0 fs-5 pb-2 mb-0">Web Developer</h3>
-                                    <p>We are in need of a Web Developer for our company.</p>
-                                    <div class="bg-light p-3 border">
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                            <span class="ps-1">Noida</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                            <span class="ps-1">Remote</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-usd"></i></span>
-                                            <span class="ps-1">2-3 Lacs PA</span>
-                                        </p>
-                                    </div>
-
-                                    <div class="d-grid mt-3">
-                                        <a href="job-detail.html" class="btn btn-primary btn-lg">Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card border-0 p-3 shadow mb-4">
-                                <div class="card-body">
-                                    <h3 class="border-0 fs-5 pb-2 mb-0">Web Developer</h3>
-                                    <p>We are in need of a Web Developer for our company.</p>
-                                    <div class="bg-light p-3 border">
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-map-marker"></i></span>
-                                            <span class="ps-1">Noida</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-clock-o"></i></span>
-                                            <span class="ps-1">Remote</span>
-                                        </p>
-                                        <p class="mb-0">
-                                            <span class="fw-bolder"><i class="fa fa-usd"></i></span>
-                                            <span class="ps-1">2-3 Lacs PA</span>
-                                        </p>
-                                    </div>
-
-                                    <div class="d-grid mt-3">
-                                        <a href="job-detail.html" class="btn btn-primary btn-lg">Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                                                 
+                        </div> 
+                        @endforeach
+                        @else
+                        <div class="col-md-12">Jobs not Found</div>
+                        @endif                     
                     </div>
                     </div>
                 </div>
@@ -250,6 +125,46 @@
 @endsection
 
 @section('customJs')
+<script>
+    $("#searchForm").submit(function(e){
+    e.preventDefault();
+    var url = '{{ route("jobs") }}?';
+
+    var keyword =$("#keyword").val();
+    var location =$("#location").val();
+    var category =$("#category").val();
+    var experience =$("#experience").val();
+    var sort = $("#sort").val();
+
+   var checkedJobTypes = $("input:checkbox[name='job_type']:checked").map(function(){
+        return $(this).val();
+    }).get();
+
+    //if keyword has a value
+    if(keyword != "") {
+        url +='&keyword='+keyword;
+    }
+     //if location has a value
+    if(location != "") {
+        url +='&location='+location;
+    }
+    //if category has a value
+    if(category != "") {
+        url +='&category='+category;
+    }
+    //if experience has a value
+    if(experience != "") {
+        url +='&experience='+experience;
+    }
+    //
+    if(checkedJobTypes.length > 0) {
+        url +='&jobType=' +checkedJobTypes;
+    }
+
+    url += '&sort='+sort;
+    window.location.href=url;
+    });
+</script>
 @endsection
 
 
